e3734f09cb3a5751b5a7226fb6c33af02259455d
student <student@student-HP-Pro-3330-MT>
2012-11-27 12:34:01 +0530

    -first commit

---
+++ sample/index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		
+		
+		
+		
+	 <link href="css/bootstrap.css" rel="stylesheet"> 
+	 <link href="css/bootstrap.min.css" rel="stylesheet">
+     <link href="css/bootstrap-responsive.css" rel="stylesheet">
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	
+        
+     <script src="js/bootstrap.js"></script>
+       <script src="js/bootstrap.min.js"></script>
+     <script src="htttp://code.jquery.com/jquery-latest.js"></script>
+<style>
+div.footer{
+	background:#e6e678;
+	}
+</style>
+	</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top">
+   <div class="navbar-inner">
+			<div class="container">
+				<button class="btn btn-navbar" data-target=".nav-collapse" data-toggle="collapse" type="button">
+                              <span class="icon-bar"></span>
+                              <span class="icon-bar"></span>
+                              <span class="icon-bar"></span>
+                         </button>
+									<div class="nav-collapse collapse">
+						<ul class="nav">
+						<li class="">
+						<form class="form-search pull-right" action="">
+							<div class="input-append">
+                       <input type="text" class="span2 search-query">
+                        <button type="submit" class="btn">Search</button>
+                        </div>
+                       </form>
+						</li>
+						</ul>
+						<ul class="nav pull-right">
+						<li class="home pull-right">
+							<a>Home</a>
+						</li>
+						<li class="abourt pull-right">
+						<a>About us</a>
+						</li>
+						<li class="contact pull-right">
+						<a>Contact</a>
+						</li>
+						</ul>
+					
+                    
+					</div>
+		
+        </div>
+        
+      
+			 </div>
+      </div>
+ </div>
+</div>
+<div class="container">
+	<br><br><br>
+						<img src="1.jpg">
+        <h1>Hello, welcome to my Site</h1>
+</div>
+<div class="navbar navbar-inverse">
+   <div class="navbar-inner">
+			<div class="container">
+				<button class="btn btn-navbar" data-target=".nav-collapse" data-toggle="collapse" type="button">
+                              <span class="icon-bar"></span>
+                              <span class="icon-bar"></span>
+                              <span class="icon-bar"></span>
+                         </button>
+   					<div class="nav-collapse collapse">
+						<ul class="nav">
+						<li class="">
+						<a>menu</a>
+						</li>
+						<li class="">
+						<a>edit</a>
+						</li>
+						<li class="">
+						<a>windows</a>
+						</li>
+						<li class="">
+						<a>project</a>
+						</li>
+						
+						</ul>
+					
+					</div>
+			 </div>
+      </div>
+ </div>
+
+<div class="container">
+	<div class="hero-unit">
+           <div id="this-carousel-id" class="carousel slide">
+
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="2.jpg" alt="" />
+      <div class="carousel-caption">
+        <p>Caption text here</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="3.jpg" alt="" />
+      <div class="carousel-caption">
+        <p>Caption text here</p>
+      </div>
+    </div>
+     <div class="item">
+      <img src="4.jpg" alt="" />
+      <div class="carousel-caption">
+        <p>Caption text here</p>
+      </div>
+    </div>
+ 
+    <a class="carousel-control left" href="#this-carousel-id" data-slide="prev">&lsaquo;</a>
+    <a class="carousel-control right" href="#this-carousel-id" data-slide="next">&rsaquo;</a>
+
+</div>
+</div>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
+
+    
+    <script src="js/bootstrap.js"></script>
+
+    <script>
+      $(document).ready(function(){
+        $('.carousel').carousel({
+          interval: 4000
+        });
+      });
+    </script>
+        </div>
+	 </div>
+ <div class="container">
+	 <div class="row-fluid">
+	 <div class="span12">
+  <div class="span4">           
+      			<h3> section </h3>
+        <p>A Palestinian worker shovels sand as he repairs a damaged smuggling tunnel dug beneath the Egyptian-Gaza border in Rafah, in the southern Gaza Strip November 26, 2012. Knee-deep in craters carved out by Israeli air strikes, Palestinians wielded shovels and planks to reopen tunnels used to smuggle in goods from Egypt to Gaza, as international aid agencies raced to replenish Gaza's supplies. REUTERS/Mohammed Salem</p>
+		<p><a class="btn btn-primary btn-small">Read more</a></p>    
+   </div>
+               <div class="span4">
+				   <h3> section </h3>
+        <p> Palestinian worker shovels sand as he repairs a damaged smuggling tunnel dug beneath the Egyptian-Gaza border in Rafah, in the southern Gaza Strip November 26, 2012. Knee-deep in craters carved out by Israeli air strikes, Palestinians wielded shovels and planks to reopen tunnels used to smuggle in goods from Egypt to Gaza, as international aid agencies raced to replenish Gaza's supplies. REUTERS/Mohammed Salem </p>
+		<p><a class="btn btn-primary btn-small">Read more</a></p>   
+                </div>
+            <div class="span4">
+				<h3> section </h3>
+        <p>A Palestinian worker shovels sand as he repairs a damaged smuggling tunnel dug beneath the Egyptian-Gaza border in Rafah, in the southern Gaza Strip November 26, 2012. Knee-deep in craters carved out by Israeli air strikes, Palestinians wielded shovels and planks to reopen tunnels used to smuggle in goods from Egypt to Gaza, as international aid agencies raced to replenish Gaza's supplies. REUTERS/Mohammed Salem </p>
+		<p><a class="btn btn-primary btn-small">Read more</a></p> 
+                </div>     
+              </div>
+              </div>
+  </div>        
+ </div>
+ <hr>
+ <div class="container">
+	 <div class="row-fluid">
+	 <div class="span12">
+ <div class="span3">
+	 <p>  aid agencies raced to replenish Gaza's supplies. REUTERS/Mohammed Salem</p>
+		<p><a class="btn btn-primary btn-small">Read more</a></p>   
+ </div>
+ 
+  <div class="span6">
+ <p>A Palestinian worker shovels sand as he repairs a damaged smuggling tunnel dug beneath the Egyptian-Gaza border in Rafah, in the southern Gaza Strip November 26, 2012. Knee-deep in craters carved out by Israeli air strikes, Palestinians wielded shovels and planks to reopen tunnels used to smuggle in goods from Egypt to Gaza, as international aid agencies raced to replenish Gaza's supplies. REUTERS/Mohammed Salem</p>
+		<p><a class="btn btn-primary btn-small">Read more</a></p>   
+ </div>
+ <div class="span3">
+	 <p>m Egypt to Gaza, as international aid agencies raced to replenish Gaza's supplies. REUTERS/Mohammed Salem</p>
+		<p><a class="btn btn-primary btn-small">Read more</a></p>   
+ </div>
+ </div>
+ </div>
+ </div>
+ <hr>
+  <div class="footer"> 
+ 
+ <div class= "container">
+ <div class="row-fluid">
+ <div class="span12">
+ <div class="span4">
+ <h3>menu</h3>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a><a class="brand" href="./index.html">Hi this is my wedsite example</a>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a>
+ </div>
+  <div class="span4">
+ <h3>menu1</h3>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a><a class="brand" href="./index.html">Hi this is my wedsite example</a>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a>
+ </div>
+  <div class="span4">
+ <h3>menu2</h3>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a><a class="brand" href="./index.html">Hi this is my wedsite example</a>
+<a class="brand" href="./index.html">Hi this is my wedsite example</a>
+ </div>
+ </div>
+ </div>
+ </div>
+ </div>
+
+</body>
+</html>
